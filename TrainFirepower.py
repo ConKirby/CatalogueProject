8434b33cd0a05ff9cb1da45ce65a9ac85738bb43
ConKirby <conckirby@gmail.com>
2026-03-09 14:38:26 +0000

    First training

--- TrainFirepower.py
+++ TrainFirepower.py
@@ -7,19 +7,23 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_percentage_error, r2_score
 import pandas as pd
 
+df = pd.read_csv(r"countries.csv")
 
-df1 = pd.read_csv(r"countries.csv")
+# Preprocessing to remove the N/A lines from Firepower
+df = df.replace("N/A", pd.NA)
+df = df.dropna()
+
+print(df.head)
+
 
 # Not needed for the model + and what we want to predict
-X = df1.drop(columns=['Country'])
+X = df.drop(columns=['Country', '% Under 18', 'Firepower Index'])
 # Target on y axis
-y = df1['2022']
+y = df['Firepower Index']
 
 # split the data into train and test
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-from sklearn.ensemble import RandomForestRegressor
 rfr = RandomForestRegressor()
 rfr.fit(X_train, y_train)
 rfr_prediction = rfr.predict(X_test)
@@ -30,10 +34,17 @@ print(f'mean_absolute_percentage_error: {rfr_mape}')
 rfr_r2 = r2_score(y_test, rfr_prediction)
 print(f'R squared: {rfr_r2}')
 
-'''import plotly.express as px
-fig = px.scatter(x=y_test, y=rfr_prediction, labels={'x': 'Actual Price', 'y': 'Predicted Price'},  title='Random Forest Regressor Model Prediction')
-fig.show()'''
+import matplotlib.pyplot as plt                                                   
+                                                                                
+feature_importance = pd.Series(rfr.feature_importances_, index=X.columns)
+feature_importance = feature_importance.sort_values(ascending=True)
+
+feature_importance.plot(kind='barh')
+plt.title('Feature Importance for Firepower Index')
+plt.tight_layout()
+plt.show()
+
 
 import joblib
 #saving model
-joblib.dump(rfr, r'path\to\trained_models\rfr_model.joblib')
+joblib.dump(rfr, r'models//rfr.joblib')
