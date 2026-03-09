22b497c132dd42d3daf05638a6097732198c89a8
ConKirby <conckirby@gmail.com>
2026-03-09 15:33:49 +0000

    predicts firepower

--- Main.java
+++ Main.java
@@ -109,13 +109,63 @@ public class Main{
     }
     public static void main(String[] args) {
         String[][] data = pullCSV();
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
 
-        // print the 2D array
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[i].length; j++) {
-                System.out.print(data[i][j] + " ");
+        while (running) {
+            System.out.println("\n--- Country Catalogue ---");
+            System.out.println("1. View all countries");
+            System.out.println("2. Add a country");
+            System.out.println("3. Remove a country");
+            System.out.println("4. Predict Firepower Index");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+
+            String choice = sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                    for (int i = 0; i < data.length; i++) {
+                        for (int j = 0; j < data[i].length; j++) {
+                            System.out.print(data[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case "2":
+                    data = addCountry(data);
+                    break;
+                case "3":
+                    Main m = new Main();
+                    data = m.removeCountry(data);
+                    break;
+                case "4":
+                    // show countries with N/A firepower
+                    String[] naCountries = UseRFR.getNACountries(data);
+                    if (naCountries.length == 0) {
+                        System.out.println("No countries with missing Firepower Index.");
+                        break;
+                    }
+                    System.out.println("\nCountries with N/A Firepower Index:");
+                    for (int i = 0; i < naCountries.length; i++) {
+                        System.out.println((i + 1) + ". " + naCountries[i]);
+                    }
+                    System.out.print("Select a country (number): ");
+                    int selection = Integer.parseInt(sc.nextLine());
+                    if (selection < 1 || selection > naCountries.length) {
+                        System.out.println("Invalid selection.");
+                        break;
+                    }
+                    String result = UseRFR.predict(naCountries[selection - 1], data);
+                    System.out.println(result);
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option.");
             }
-            System.out.println();
         }
+        sc.close();
     }
 }
--- predict_firepower.py
+++ predict_firepower.py
@@ -1,12 +1,12 @@
 import pandas as pd                                                               
 import joblib    
-                                                                 
-def use_rfr(Country,GDPPerCapita,Population,MalesPer100Females,perc18AndOver,Incar  ceratedPer100k,CorporationTax):                                                   
-    X1 = [Country,GDPPerCapita,Population,MalesPer100Females,perc18AndOver,IncarceratedPer100k,CorporationTax]
+
+def use_rfr(GDPPerCapita,Population,MalesPer100Females,perc18AndOver,IncarceratedPer100k,CorporationTax):                                                   
+    X1 = [GDPPerCapita,Population,MalesPer100Females,perc18AndOver,IncarceratedPer100k,CorporationTax]
     rfr = joblib.load(r'models\rfr.joblib')
 
     #rfr_predictions1 = rfr.predict(X1)
-    rfr_prediction = rfr.predict(X1)
+    rfr_prediction = rfr.predict([X1])
 
     return rfr_prediction
 
